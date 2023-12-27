657a4723e147c9c439ef23ab53b6a86fcf00ef13
Andrey <andryuxa16@yandex.ru>
2023-12-27 19:04:08 +0300

    add commit Employee.java,add commit Main.java

---
+++ src/Employee.java
@@ -0,0 +1,101 @@
+
+
+    public class Employee {
+        private String SurName;
+
+        private String Name;
+
+        private String Patronymic;
+
+        private int Department;
+
+        private int Salary;
+
+        private final int id;
+
+        private static int counter;
+
+        public String getSurName() {
+            return SurName;
+        }
+
+        public void setSurName(String surName) {
+            SurName = surName;
+        }
+
+        public String getName() {
+            return Name;
+        }
+
+        public void setName(String name) {
+            Name = name;
+        }
+
+        public String getPatronymic() {
+            return Patronymic;
+        }
+
+        public void setPatronymic(String patronymic) {
+            Patronymic = patronymic;
+        }
+
+        public int getDepartment() {
+            return Department;
+        }
+
+        public void setDepartment(int department) {
+            Department = department;
+        }
+
+        public int getSalary() {
+            return Salary;
+        }
+
+        public void setSalary(int salary) {
+            Salary = salary;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public static int getCounter() {
+            return counter;
+        }
+
+        public static void setCounter(int counter) {
+            Employee.counter = counter;
+        }
+
+        public Employee(String surName, String name, String patronymic, int department, int salary) {
+            if (department <= 0 || department > 5)
+                throw new IllegalArgumentException("Такого сотрудника не существует");
+            this. counter++;
+            this.SurName = surName;
+            this.Name = name;
+            this. Patronymic = patronymic;
+            this.Department = department;
+            this.Salary = salary;
+            this.id = getCounter();
+        }
+
+        @Override
+        public String toString() {
+            return "" +
+                    "Фамилия-" + SurName + "," +
+                    " Имя-" + Name + "," +
+                    " Отчество-" + Patronymic + "," +
+                    " Отдел-" + Department + ":" +
+                    " Зарплата-" + Salary +
+                    " id=" + id +
+                    "";
+        }
+        public String getFullName(){
+            return SurName + " " + Name +" "+ Patronymic;
+        }
+    }
+
+
+
+
+
